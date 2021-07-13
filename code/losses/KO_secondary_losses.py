67b9eca520466b7eb5d629e8c20124a37271d901
alonfa <alondrazita@gmail.com>
2021-07-14 00:10:24 +0200

    update 13/07/2021

--- code/losses/KO_secondary_losses.py
+++ code/losses/KO_secondary_losses.py
@@ -14,7 +14,7 @@ Descpription: functions for calculation of secondary losses within Kacker-Okapuu
 ## IMPORT NECESSARY LIBRARIES
 import numpy as np                   # library for math and calculation functions
 
-from KC_profile_losses import losses_KP
+from KO_profile_losses import losses_KP
 
 def secondary_losses_fAR(h_c):
 
@@ -26,38 +26,38 @@ def secondary_losses_fAR(h_c):
     return fAR
 
 
-def secondary_losses_alpham(alpha2, alpha3):
-    alpham = np.arctan(0.5*(np.tan(alpha2)-np.tan(alpha3)))
+def secondary_losses_alpham(alpha_in, alpha_out):
+    alpham = np.arctan(0.5*(np.tan(alpha_in)-np.tan(alpha_out)))
     return alpham
 
-def secondary_losses_AMDC(alpha2, alpha3, h_c):
-    ## confirm equations are correct with the KTH thesis
+
+def secondary_losses_AMDC(alpha_in, alpha_out, h_c):
 
     fAR = secondary_losses_fAR(h_c)
 
-    alpham = secondary_losses_alpham(alpha2, alpha3)
-    C1 = 2*(np.tan(alpha2)-np.tan(alpha3))*np.cos(alpham)
+    alpham = secondary_losses_alpham(alpha_in, alpha_out)
+    C1 = 2*(np.tan(alpha_in)+np.tan(alpha_out))*np.cos(alpham)
 
-    YS_AMDC = 0.0334*fAR*np.cos(alpha3)/np.cos(alpha2)*C1**2*np.cos(alpha3)**2/np.cos(alpham)**3
+    YS_AMDC = 0.0334*fAR*np.cos(alpha_out)/np.cos(alpha_in)*C1**2*np.cos(alpha_out)**2/np.cos(alpham)**3
 
     return YS_AMDC
 
 
-def secondary_losses_Ks(bx, h, M2, M3):
+def secondary_losses_Ks(bx, h, M_in, M_out):
 
     K3 = (bx/h)**2
-    KP = losses_KP(M2, M3)
+    KP = losses_KP(M_in, M_out)
 
     Ks = 1 - K3*(1-KP)
 
     return Ks
 
 
-def secondary_losses(alpha2, alpha3, h_c, bx, h, M2, M3):
+def secondary_losses(alpha_in, alpha_out, h_c, bx, h, M_in, M_out):
 
 
-    YS_AMDC = secondary_losses_AMDC(alpha2, alpha3, h_c)
-    Ks = secondary_losses_Ks(bx, h, M2, M3)
+    YS_AMDC = secondary_losses_AMDC(alpha_in, alpha_out, h_c)
+    Ks = secondary_losses_Ks(bx, h, M_in, M_out)
 
 
     YS = 1.2*YS_AMDC*Ks
---
+++ code/losses/saves/save10_ fixed pitch and stagger/solve_functions.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 15:05:31 2021
+
+@author: alondra sola
+
+Project: turbine design tool (TFG)
+Program: 1D analysis (imitating the "automobile gas generator" design tool)
+File: solve_functions.py
+Description: contains the implementation of all the functions needed for
+convergence with non-linear solvers
+
+"""
+
+import numpy as np                   # library for math and calculation functions
+from scipy.optimize import fsolve, least_squares
+import aux_functions as f
+
+
+def solve_rhox(rho_init, rho0, T0, mdot, Ay, gamma, cp):
+    """
+    Function to find the density rho for a certain point x, from the
+    temperature, mass flux, and area data.
+    Using an initial guess rhox_init, which is always less than rho0x
+    """
+
+    def f_rho(rho, rho0, T0, mdot, Ay, gamma, cp):
+        V = np.sqrt(2*cp*T0*(1-(rho/rho0)**(gamma-1)))
+        A = mdot/rho/V
+        return Ay-A
+
+    rho = fsolve(f_rho,rho_init,args=(rho0, T0, mdot, Ay, gamma, cp))[0]
+    V = np.sqrt(2*cp*T0*(1-(rho/rho0)**(gamma-1)))
+    A = mdot/rho/V
+
+    return rho, V, A;
+
+
+
+def solve_angles(angles_init, one, two, thr, stator, rotor, gamma, cp, R, GR, psi, DeltaH_prod, bounds_angles):
+
+    def f_angles(angles, one, two, thr, stator, rotor, gamma, cp, R, GR, psi, DeltaH_prod):
+
+        alpha = angles[0]
+        beta = angles[1]
+
+        # assume an two.alpha and project velocities
+
+        two.vel.Vx, two.vel.Vu = f.velocity_projections(two.vel.V, alpha)
+
+        # assume a loading factor and calculate peripheral speed
+        two.vel.U = np.sqrt(DeltaH_prod/psi)
+
+        # velocity triangle (pithagoras)
+        two.vel.Wu = two.vel.Vu - two.vel.U
+        two.vel.Wx = two.vel.Vx
+        two.vel.W = f.mag(two.vel.Wu, two.vel.Wx)
+
+        # relative inlet angle and relative Mach number
+        two.vel.Mr = two.vel.W/two.vel.a
+
+        # relative total quantities at rotor inlet
+        two.T0r, two.P0r = f.relative_temperature_pressure(two.T, two.P, two.vel.Mr, gamma)
+
+        ############ ROTOR #############
+        # in the rotor, the relative total temperature is constant (se conservan rotalpías)
+        thr.T0r = two.T0r
+
+        # ideal outlet temperature, real outlet temperature
+        # assuming the expanse to thr.P, calculated with the assumed M3
+        thr.Ts = f.P2T(thr.T0r, thr.P, two.P0r, gamma)                  # = thr.T0r*(thr.P/two.P0r)**((gamma-1)/gamma)
+        thr.T = f.stage_efficiency(rotor.eta, thr.T0r, thr.Ts)       # = thr.T0r - rotor.eta*(thr.T0r - thr.Ts)
+
+        # velocities
+        thr.vel.W = f.isen_velocity(cp, thr.T0r, thr.T)                  # = np.sqrt(2*cp*(thr.T0r - thr.T))
+
+        # velocity projections, axial and tangential
+        thr.vel.Wx, thr.vel.Wu = f.velocity_projections(thr.vel.W, beta)
+
+        # constant radius, constant peripheral speed
+        thr.vel.U = two.vel.U
+
+        # tangential outlet speed
+        thr.vel.Vu = thr.vel.Wu + thr.vel.U
+        # V3u_euler = -DeltaH_prod/two.vel.U + two.vel.Vu
+
+
+        # find work, check for angle iteration
+        DeltaH_calculated = two.vel.U*(two.vel.Vu - thr.vel.Vu)
+
+        diff = abs(DeltaH_calculated - DeltaH_prod)
+
+
+        return np.array([diff[0], 0])
+
+    # angles  = fsolve(f_angles,angles_init,args=(one, two, thr, stator, rotor, gamma, cp, R, GR, psi, DeltaH_prod))
+    results  = least_squares(f_angles,angles_init,args=(one, two, thr, stator, rotor, gamma, cp, R, GR, psi, DeltaH_prod),bounds=bounds_angles, method='trf')
+    # (the least_squares method allows for the inclusion of bounds)
+
+    # if results.success != False:
+    #     print('ERROR: The angle conversion was not successful (function solve_angles in solve_functions.py)')
+
+    return results.x
+
+
