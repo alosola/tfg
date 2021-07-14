225a46319d5348b18e43653fe21048dfefd968d8
alonfa <alondrazita@gmail.com>
2021-07-15 00:49:42 +0200

    chapters 1-4 complete

--- code/losses/kackerokapuu_plot.py
+++ code/losses/kackerokapuu_plot.py
@@ -18,37 +18,37 @@ import scipy.interpolate
 
 
 ############ PLOT FOR FIGURE YP ALPHA IN = 0
-# x = np.genfromtxt('fig1_x.csv', delimiter=',')               # import SC vector
-# y = np.flip(np.genfromtxt('fig1_y.csv', delimiter=','))      # import alpha2 vector
-# z = np.flip(np.genfromtxt('fig1_Zm.csv', delimiter=','), 1)  # import YP mesh
+x = np.genfromtxt('fig1_x.csv', delimiter=',')               # import SC vector
+y = np.flip(np.genfromtxt('fig1_y.csv', delimiter=','))      # import alpha2 vector
+z = np.flip(np.genfromtxt('fig1_Zm.csv', delimiter=','), 1)  # import YP mesh
 
-# YP_spline =  scipy.interpolate.RectBivariateSpline(x, y, z)  # create spline for evaluation
+YP_spline =  scipy.interpolate.RectBivariateSpline(x, y, z)  # create spline for evaluation
 
 
-# N = 500
-# X = np.linspace(np.amin(x),np.amax(x),N)
-# Y = np.linspace(np.amin(y),np.amax(y),N)
-# Z = np.zeros((N,N))
+N = 500
+X = np.linspace(np.amin(x),np.amax(x),N)
+Y = np.linspace(np.amin(y),np.amax(y),N)
+Z = np.zeros((N,N))
 
-# for i in range(N):
-#     for j in range(N):
-#         Z[i,j] = YP_spline(X[i],Y[j])
+for i in range(N):
+    for j in range(N):
+        Z[i,j] = YP_spline(X[i],Y[j])
 
-# Xgrid = np.zeros((N,N))
-# Ygrid = np.zeros((N,N))
-# for i in range(N):
-#     Xgrid[:,i] = X
-#     Ygrid[i,:] = Y
+Xgrid = np.zeros((N,N))
+Ygrid = np.zeros((N,N))
+for i in range(N):
+    Xgrid[:,i] = X
+    Ygrid[i,:] = Y
 
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.set_xlabel('Pitch-to-chord ratio s/c [-]')
-# ax.set_ylabel(r'Inlet angle $\alpha$ [deg]')
-# ax.set_zlabel(r'Pressure loss coefficient $Y_P$ [-]')
-# # ax.plot_wireframe(Xgrid, Ygrid, Z, color='black')
-# ax.plot_surface(Xgrid, Ygrid, Z, cstride=1, rstride=1, cmap='viridis', edgecolor='none')
-# ax.set_title('Surface plot')
-# plt.show()
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.set_xlabel('Pitch-to-chord ratio s/c [-]')
+ax.set_ylabel(r'Inlet angle $\alpha$ [deg]')
+ax.set_zlabel(r'Pressure loss coefficient $Y_P$ [-]')
+# ax.plot_wireframe(Xgrid, Ygrid, Z, color='black')
+ax.plot_surface(Xgrid, Ygrid, Z, cstride=1, rstride=1, cmap='magma_r', edgecolor='none')
+ax.set_title('Surface plot')
+plt.show()
 
 
 
@@ -313,33 +313,33 @@ import scipy.interpolate
 
 ######## STAGGER ANGLE
 
-x = np.genfromtxt('stg_x.csv', delimiter=',')               # import SC vector
-y = np.genfromtxt('stg_y.csv', delimiter=',')      # import alpha2 vector
-z = np.flip(np.genfromtxt('stg_Zm.csv', delimiter=','),0)  # import YP mesh
+# x = np.genfromtxt('stg_x.csv', delimiter=',')               # import SC vector
+# y = np.genfromtxt('stg_y.csv', delimiter=',')      # import alpha2 vector
+# z = np.flip(np.genfromtxt('stg_Zm.csv', delimiter=','),0)  # import YP mesh
 
-YP_spline =  scipy.interpolate.RectBivariateSpline(x, y, z)  # create spline for evaluation
+# YP_spline =  scipy.interpolate.RectBivariateSpline(x, y, z)  # create spline for evaluation
 
-N = 60
-X = np.linspace(np.amin(x),np.amax(x),N)
-Y = np.linspace(np.amin(y),np.amax(y),N)
-Z = np.zeros((N,N))
+# N = 60
+# X = np.linspace(np.amin(x),np.amax(x),N)
+# Y = np.linspace(np.amin(y),np.amax(y),N)
+# Z = np.zeros((N,N))
 
-for i in range(N):
-    for j in range(N):
-        Z[i,j] = YP_spline(X[i],Y[j])
+# for i in range(N):
+#     for j in range(N):
+#         Z[i,j] = YP_spline(X[i],Y[j])
 
-Xgrid = np.zeros((N,N))
-Ygrid = np.zeros((N,N))
-for i in range(N):
-    Xgrid[:,i] = X
-    Ygrid[i,:] = Y
+# Xgrid = np.zeros((N,N))
+# Ygrid = np.zeros((N,N))
+# for i in range(N):
+#     Xgrid[:,i] = X
+#     Ygrid[i,:] = Y
 
-fig = plt.figure()
-# ax = plt.axes()
-# ax.plot_surface(Xgrid, Ygrid, Z, cstride=1, rstride=1, cmap='viridis', edgecolor='none')
-plt.xlabel(r'Inlet angle $\alpha_{in}$ [deg]')
-plt.ylabel(r'Outlet angle $\alpha_{out}$ [deg]')
-# plt.zlabel(r'Stagger angle $\varphi$ [deg]')
-plt.contourf(Xgrid, Ygrid, Z, 15, cmap='BuPu')
-plt.colorbar()
-plt.show()
+# fig = plt.figure()
+# # ax = plt.axes()
+# # ax.plot_surface(Xgrid, Ygrid, Z, cstride=1, rstride=1, cmap='viridis', edgecolor='none')
+# plt.xlabel(r'Inlet angle $\alpha_{in}$ [deg]')
+# plt.ylabel(r'Outlet angle $\alpha_{out}$ [deg]')
+# # plt.zlabel(r'Stagger angle $\varphi$ [deg]')
+# plt.contourf(Xgrid, Ygrid, Z, 15, cmap='magma_r')
+# plt.colorbar()
+# plt.show()
